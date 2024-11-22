52de10744e165ed7f6655f19ce5f80cfed140393
AlexOS12 <sluhach2016@gmail.com>
2024-11-22 17:54:45 +0300

    1 задание ЛР4 готово

--- lab4/Adapter/Adapter.h
+++ lab4/Adapter/Adapter.h
@@ -1,1 +1,82 @@
 #pragma once
+
+#include <string>
+
+// Класс баллона с газом
+class GasCylinder {
+protected:
+	double volume; // объем баллона в м^3
+	double mass;   // масса газа в кг
+	double molar;  // молярная масса газа в кг/ммоль
+
+public:
+	GasCylinder() {
+		volume = 0.0;
+		mass = 0.0;
+		molar = 0.0;
+	}
+
+	GasCylinder(double volume, double mass, double molar) {
+		this->volume = volume;
+		this->mass = mass;
+		this->molar = molar;
+	}
+
+	// Возвращает текущее давление в баллоне
+	double getPressure(int t) {
+		// 8.31 - число R
+		double pressure = (amountOfMatter() * 8.31 * t) / volume;
+		return pressure;
+	};
+
+	// Возвращает кол-во вещества в баллоне
+	double amountOfMatter() {
+		double amount = (mass * 1'000) / molar;
+		return amount;
+	};
+
+	// Возвращает строку с сведениями о баллоне
+	std::string toString() {
+		std::string data = "Сведения о газе, содержащемся в баллоне:\n";
+		data += "Объем баллона: " + std::to_string(volume) + " м^3\n";
+		data += "Масса газа: " + std::to_string(mass) + " кг\n";
+		data += "Молярная масса газа: " + std::to_string(molar) + " г/моль\n";
+		data += "Количество газа: " + std::to_string(amountOfMatter()) + " моль\n";
+		return data;
+	};
+};
+
+// Класс интерфейс
+class ITarget {
+public:
+	// Изменяет объем баллона на величину dV
+	virtual void modifyVolume(double dV) = 0 ;
+	// Определяет изменение давления при изменении температуры
+	virtual double getDp(int t0, int t1) = 0;
+	// Получить сведения о баллоне
+	virtual std::string passport() = 0;
+};
+
+// Класс адаптера
+class Adapter : public ITarget, private GasCylinder {
+public:
+	Adapter(double volume, double mass, double molar) {
+		this->volume = volume;
+		this->mass = mass;
+		this->molar = molar;
+	}
+
+	void modifyVolume(double dV) override {
+		this->volume += dV;
+	}
+
+	double getDp(int t0, int t1) override {
+		double pressureT0 = getPressure(t0);
+		double pressureT1 = getPressure(t1);
+		return pressureT1 - pressureT0;
+	}
+
+	std::string passport() override {
+		return toString();
+	}
+};
--- lab4/Adapter/main.cpp
+++ lab4/Adapter/main.cpp
@@ -4,5 +4,13 @@
 int main() {
 	setlocale(LC_ALL, "Russian");
 
+	ITarget* target = new Adapter(1.0, 1.0, 32.0);
+
+	std::cout << target->passport() << "\n";
+	target->modifyVolume(2);
+	std::cout << target->passport() << "\n";
+
+	std::cout << "Разница в давлении между 22°C и 30°C: " << target->getDp(22, 30) << " Па\n";
+
 	return 0;
 }
