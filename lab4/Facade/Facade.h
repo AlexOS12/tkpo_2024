e6c57a9524e66f5c213ebfb8925eeb0708b789c0
AlexOS12 <sluhach2016@gmail.com>
2024-11-22 18:52:02 +0300

    ЛР4 готова

--- lab4/Facade/Facade.h
+++ lab4/Facade/Facade.h
@@ -1,1 +1,58 @@
 #pragma once
+
+#include <iostream>
+
+namespace subsystem {
+	// Базовый класс недвижимости
+	class RealEstateType {
+	public:
+		// Базовый платеж
+		virtual int getBasePrice() = 0;
+	};
+
+	// Класс квартиры
+	class Flat : public RealEstateType {
+		int getBasePrice() override {
+			return 3500;
+		}
+	};
+
+	// Класс таунхауса
+	class TownHouse : public RealEstateType {
+		int getBasePrice() override {
+			return 2700;
+		}
+	};
+
+	// Класс коттеджа 
+	class Cottage : public RealEstateType {
+		int getBasePrice() override {
+			return 4200;
+		}
+	};
+
+}
+
+// Фасад для расчета стоимости страхового взноса
+class Facade {
+protected:
+	// Тип недвижимости
+	subsystem::RealEstateType* realEstate;
+	// Площадь недвижимости
+	int area;
+	// Износ недвижимости
+	int wear;
+	
+public:
+	Facade(subsystem::RealEstateType* realEstate, int area, int wear) {
+		this->realEstate = realEstate;
+		this->area = area;
+		this->wear = wear;
+	}
+
+	int calculateInsurancePrice() {
+		double wearCoefficient = 1.0 + ((double)wear / 100.0);
+		int insurancePrice = realEstate->getBasePrice() * area * wearCoefficient;
+		return insurancePrice;
+	}
+};
--- lab4/Facade/main.cpp
+++ lab4/Facade/main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include "Facade.h"
+
+int insurancePriceCalculator(subsystem::RealEstateType* realEstateType, int area, int wear) {
+	Facade* facade = new Facade(realEstateType, area, wear);
+	int price = facade->calculateInsurancePrice();
+	delete facade;
+	return price;
+}
 
 int main() {
 	setlocale(LC_ALL, "Russian");
 
+	std::cout << "Стоимость страхового взноса за:\n";
+
+	subsystem::RealEstateType* flat = new subsystem::Flat;
+	std::cout << "Кватиру:  " << insurancePriceCalculator(flat, 60, 10) << "\n";
+	delete flat;
+	subsystem::RealEstateType* townHouse = new subsystem::TownHouse;
+	std::cout << "Таунхаус: " << insurancePriceCalculator(townHouse, 60, 10) << "\n";
+	delete townHouse;
+	subsystem::RealEstateType* cottage = new subsystem::Cottage;
+	std::cout << "Коттедж:  " << insurancePriceCalculator(cottage, 60, 10) << "\n";
+	delete cottage;
+	
 	return 0;
 }
